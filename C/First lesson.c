b3b473cf3cd9d2d47824909d9a655dc007ca31a4
Sandro Figueira <sandrofigueirajus@hotmail.com>
2022-09-22 00:00:13 -0300

    Create

---
+++ C/First lesson.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int a = 10; //declarando variável "a" e atribuindo-lhe o valor inteiro 10
+    int b = 20; // declarando variável "b" e atribuindo-lhe o valor inteiro 20
+    int resultado = a + b; // declara a variável "resultado" e lhe atribuiu o valor soma a + b
+
+    /*
+    Comentários maior do que uma linha
+    */
+   
+    printf("Resultado: %d", resultado);
+   return 0;
+
+
+}
